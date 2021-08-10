5ac710d4b9e0c19a228e8b153d26f0a00bd79e9e
n-duku <ebenezer.duku@amalitech.org>
2021-08-10 12:28:38 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+let userName = '';
+
+userName === 'Jane'? console.log(`Hello ${userName}!`) : console.log('Hello!');
+
+let userQuestion = 'What is my age?';
+console.log(userQuestion);
+
+let randomNumber = Math.floor(Math.random() * 7);
+let eightBall = '';
+
+switch(randomNumber){
+  case 0:
+    eightBall = 'It is certain';
+    break;
+  case 1:
+    eightBall = 'It is decidedly so';
+    break;
+  case 2:
+    eightBall = 'Reply hazy try again';
+    break;
+  case 3:
+    eightBall = 'Cannot predict now';
+    break;
+  case 4:
+    eightBall = 'Do not count it';
+    break;
+  case 5:
+    eightBall = 'My sources say no';
+    break;
+  case 6:
+    eightBall = 'Outlook not so good';
+    break;
+  case 7:
+    eightBall = 'Signs point to yes';
+    break;
+}
+
+console.log(eightBall);
+
